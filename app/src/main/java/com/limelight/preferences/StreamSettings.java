a87681bdf6776f21fe4be093c77a5c7080bbdc8f
wangguan <1205854813@qq.com>
2024-11-26 11:05:03 +0800

    特性：添加游戏震动和按键震动

--- app/src/main/java/com/limelight/preferences/StreamSettings.java
+++ app/src/main/java/com/limelight/preferences/StreamSettings.java
@@ -738,7 +738,7 @@ public class StreamSettings extends Activity {
                 List<Long> configIdList = superConfigDatabaseHelper.queryAllConfigIds();
                 Map<String, String> configMap = new HashMap<>();
                 for (Long configId : configIdList){
-                    String configName = (String) superConfigDatabaseHelper.queryConfigAttribute(configId, PageConfigController.COLUMN_STRING_CONFIG_NAME);
+                    String configName = (String) superConfigDatabaseHelper.queryConfigAttribute(configId, PageConfigController.COLUMN_STRING_CONFIG_NAME,"default");
                     String configIdString = String.valueOf(configId);
                     configMap.put(configIdString,configName);
                 }
@@ -769,7 +769,7 @@ public class StreamSettings extends Activity {
                 List<Long> configIdList = superConfigDatabaseHelper.queryAllConfigIds();
                 Map<String, String> configMap = new HashMap<>();
                 for (Long configId : configIdList){
-                    String configName = (String) superConfigDatabaseHelper.queryConfigAttribute(configId, PageConfigController.COLUMN_STRING_CONFIG_NAME);
+                    String configName = (String) superConfigDatabaseHelper.queryConfigAttribute(configId, PageConfigController.COLUMN_STRING_CONFIG_NAME,"default");
                     String configIdString = String.valueOf(configId);
                     configMap.put(configIdString,configName);
                 }
@@ -848,7 +848,7 @@ public class StreamSettings extends Activity {
                                 List<Long> configIdList = superConfigDatabaseHelper.queryAllConfigIds();
                                 Map<String, String> configMap = new HashMap<>();
                                 for (Long configId : configIdList){
-                                    String configName = (String) superConfigDatabaseHelper.queryConfigAttribute(configId, PageConfigController.COLUMN_STRING_CONFIG_NAME);
+                                    String configName = (String) superConfigDatabaseHelper.queryConfigAttribute(configId, PageConfigController.COLUMN_STRING_CONFIG_NAME,"default");
                                     String configIdString = String.valueOf(configId);
                                     configMap.put(configIdString,configName);
                                 }
